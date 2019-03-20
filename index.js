22ca034739eef217970940f9e12bb3d9010adebc
Kelly <kelly.ho@sky.uk>
2019-03-20 13:07:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+
+const drivers = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendDriver() {
+  drivers.push('Ralph');
+  return drivers;
+}
+
+function destructivelyPrependDriver() {
+  drivers.unshift('Bob');
+  return drivers;
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop('Garfield');
+  return drivers;
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift('Milo');
+  return drivers;
+}
+
+//the below do not mutate the original array, returns a new array.
+
+function appendDriver() {
+  return [...drivers, 'Broom'];
+}
+
+function prependDriver() {
+  return['Arnold', ...drivers];
+}
+
+function removeLastDriver() {
+  return drivers.slice(0,2);
+}
+
+function removeFirstDriver() {
+  return drivers.slice(1);
+}
+
